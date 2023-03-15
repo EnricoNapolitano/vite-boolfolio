89d91cea78d4f29eaad1397318f53b2c80ef3d8f
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-16 00:17:00 +0100

    ProjectCard component

---
+++ src/components/projects/ProjctCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: { project: Object },
+}
+</script>
+
+<template>
+    <div class="card m-3">
+        <div class="card-header">
+            <h2>{{ project.title }}</h2>
+        </div>
+        <div class="card-body">
+            <p>{{ project.content }}</p>
+        </div>
+        <div class="card-footer">
+            <i v-for="technology in project.technologies" :key="technology.id" :class="technology.class_icon"
+                class="fa-2x me-4"></i>
+        </div>
+    </div>
+</template>
