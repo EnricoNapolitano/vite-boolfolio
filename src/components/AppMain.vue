6e3222b5bf0cb48dab1f16db4b0e8b353bba62b2
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-16 00:16:28 +0100

    importing component ProjectList in AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+import ProjectsList from './projects/ProjectsList.vue';
+export default {
+    name: 'AppMain',
+    props: { projects: Array },
+    components: { ProjectsList }
+}
+</script>
+
+<template>
+    <projects-list :projects="projects"></projects-list>
+</template>
