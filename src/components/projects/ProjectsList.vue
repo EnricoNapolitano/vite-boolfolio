fa5118bebce2f0b361386325f9fdbbe88415b4c4
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-16 00:16:45 +0100

    ProjectList Component

---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,14 @@
+<script>
+import ProjectCard from './ProjctCard.vue';
+export default {
+    name: 'ProjectsList',
+    props: { projects: Array },
+    components: { ProjectCard }
+}
+</script>
+
+<template>
+    <div v-for="project in projects" :key="project.id" class="container">
+        <project-card :project="project"></project-card>
+    </div>
+</template>
