4ccea6b3df9ab9cc056e12416d243f5643c2daf5
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-16 19:10:22 +0100

    adding flag and button to go back home (Proj.Card)

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: { project: Object },
+    props: { project: Object, isDetail: Boolean },
 }
 </script>
 
@@ -17,5 +17,13 @@ export default {
             <i v-for="technology in project.technologies" :key="technology.id" :class="technology.class_icon"
                 class="fa-2x me-4"></i>
         </div>
+
+        <!-- Button -->
+        <router-link v-if="!isDetail" :to="{ name: 'project-detail', params: { id: project.id } }"
+            class="btn btn-outline-primary">
+            <b>VIEW PROJECT!</b>
+        </router-link>
+        <router-link v-else :to="{ name: 'home' }" class="btn btn-outline-primary"><b>GO BACK TO HOME</b></router-link>
+
     </div>
 </template>
