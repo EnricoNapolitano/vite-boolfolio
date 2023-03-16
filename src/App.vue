74db2db294e7a67d376789de202c7d8b507df056
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-16 17:15:12 +0100

    changed App, deleted MainApp, created HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <script >
-import { createDOMCompilerError } from '@vue/compiler-dom';
-import axios from 'axios';
-import { onMounted } from 'vue';
+//RouterView is imported in main.js, so there's no need to import here
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
-const apiBaseUrl = 'http://localhost:8000/api/';
 export default {
   name: 'App',
-  components: { AppHeader, AppMain },
-  data() {
-    return {
-      projects: [],
-    }
-  },
-  methods: {
-    fetchProjects() {
-      axios.get(apiBaseUrl + 'projects').then(res => {
-        this.projects = res.data;
-      })
-    }
-  },
-  created() {
-    this.fetchProjects();
-  }
+  components: { AppHeader },
 };
 </script>
 
 <template>
   <app-header></app-header>
-  <app-main :projects="projects"></app-main>
+  <router-view></router-view>
 </template>
 
 <style></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+const apiBaseUrl = 'http://localhost:8000/api/';
+import ProjectsList from '../components/projects/ProjectsList.vue';
+export default {
+    name: "HomePage",
+    components: { ProjectsList },
+    data() { return { projects: [], } },
+    methods: {
+        fetchProjects() {
+            axios.get(apiBaseUrl + 'projects').then(res => {
+                this.projects = res.data;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
+}
+</script>
+<template>
+    <projects-list :projects="projects"></projects-list>
+</template>
+<style></style>
