c12010b6e20ff58717643bed419c0e93efc7c4c7
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-16 19:09:01 +0100

    ProjectDetail component

---
+++ src/components/projects/ProjectDetail.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
+const apiBaseUrl = 'http://localhost:8000/api/';
+export default {
+    name: 'ProjectDetail',
+    components: { ProjectCard },
+    data() {
+        return {
+            project: null,
+        }
+    },
+    methods: {
+        getProject() {
+            const endpoint = apiBaseUrl + 'projects/' + this.$route.params.id;
+            axios.get(endpoint).then(res => {
+                this.project = res.data;
+            });
+        },
+    },
+    created() {
+        this.getProject();
+    }
+};
+</script>
+<template>
+    <project-card :project="project" :isDetail="true"></project-card>
+</template>
