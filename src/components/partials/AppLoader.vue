ce07466b9107ac46b632af49137fed1cf774bf16
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-17 08:47:14 +0100

    created component AppLoader

---
+++ src/components/partials/AppLoader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'AppLoader'
+}
+</script>
+
+<template>
+    <div class="loader-container d-flex flex-column justify-content-center align-items-center">
+        <i class="fa-solid fa-compact-disc fa-spin fa-5x"></i>
+        <div class="loader">Loading...</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.loader-container {
+    margin-top: 200px;
+
+    .loader {
+        font-size: 2rem;
+    }
+}
+</style>
