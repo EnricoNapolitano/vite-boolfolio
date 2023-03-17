6981a7d05775a41d8bc316e847e7072e3c6954cf
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-17 08:48:23 +0100

    riorganized HomePage: loader and alert for error

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,14 +2,26 @@
 import axios from 'axios';
 const apiBaseUrl = 'http://localhost:8000/api/';
 import ProjectsList from '../components/projects/ProjectsList.vue';
+import ErrorAlert from '../components/partials/ErrorAlert.vue';
 export default {
     name: "HomePage",
-    components: { ProjectsList },
-    data() { return { projects: [], } },
+    components: { ProjectsList, ErrorAlert },
+    data() {
+        return {
+            projects: [],
+            isLoading: false,
+            hasError: false,
+        }
+    },
     methods: {
         fetchProjects() {
+            this.isLoading = true;
             axios.get(apiBaseUrl + 'projects').then(res => {
                 this.projects = res.data;
+            }).catch(() => {
+                this.hasError = true;
+            }).then(() => {
+                this.isLoading = false;
             })
         }
     },
@@ -19,6 +31,15 @@ export default {
 }
 </script>
 <template>
-    <projects-list :projects="projects"></projects-list>
+    <error-alert :hasError="hasError" @close="hasError = false"></error-alert>
+    <app-loader v-if="isLoading"></app-loader>
+
+    <!-- projects -->
+    <div v-else class="container">
+        <projects-list v-if="projects.length" :projects="projects"></projects-list>
+        <div v-else class="text-center mt-5">
+            <h2>There aren't projects to show.</h2>
+        </div>
+    </div>
 </template>
 <style></style>
